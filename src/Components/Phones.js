8389d3cd8502b147a0b2e84394dd91f259228b00
Akeiba <Akeiba@niyoenterprise.com>
2022-05-31 18:55:21 +0100

    initial commit

---
+++ src/Components/Phones.js
@@ -0,0 +1,21 @@
+import "../Styles/phones.css"
+import { FaFacebookSquare, FaInstagramSquare, FaTwitterSquare, FaLinkedin } from 'react-icons/fa';
+
+export default function Phones() {
+    return (<>
+        <div className="contact-container">
+        <div className="socials-container">
+
+        <FaFacebookSquare className="icon" />
+        <FaInstagramSquare className="icon" />
+        <FaTwitterSquare className="icon" />
+        <FaLinkedin className="icon" />
+        </div>
+
+        <div className="phones-container">
+        <img src={"/phones-image.png"} ></img>
+        </div>
+        </div>
+        </>
+    )
+};
