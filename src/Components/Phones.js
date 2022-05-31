ca349012c163e3b3a8515a726552d6126efb6b03
Akeiba <Akeiba@niyoenterprise.com>
2022-05-31 18:41:20 +0100

    made changes to social media icons

--- src/Components/Phones.js
+++ src/Components/Phones.js
@@ -1,19 +1,26 @@
-import "../Styles/phones.css"
+import "../Styles/phones.css";
+import { FaFacebookSquare, FaInstagramSquare, FaTwitterSquare, FaLinkedin } from 'react-icons/fa'; 
 
 export default function Phones() {
     return (<>
         <div className="contact-container">
         <div className="socials-container">
-        <p >facebook</p>
-        <p>instragram</p>
-        <p>twitter</p>
-        <p>linkedin</p>
+            <FaFacebookSquare className="icon"/>
+            <FaInstagramSquare className="icon" />
+            <FaTwitterSquare className="icon" />
+            <FaLinkedin className="icon" />
         </div>
 
         <div className="phones-container">
-        <img src={"/phones-image.png"} ></img>
+        <img className="phones-img" src={"/phones-image.png"} alt="image of three phones" ></img>
         </div>
         </div>
         </>
     )
 };
+
+
+/* <img className="icon" src={"/facebook.png"} alt={"facebook logo"}></img>
+<img className="icon" src={"/instagram.png"} alt={"instagram logo"}></img>
+<img className="icon" src={"/twitter.png"} alt={"twitter logo"}></img>
+<img className="icon" src={"/linkedin.png"} alt={"linkeddin logo"}></img> */
